f3a01944b5ac13515f3be56554342288068869f9
Arif Dwi N <ardwiinoo@gmail.com>
2026-01-11 20:44:34 +0700

    Add stack logic

---
+++ stack_queue/stack.go
@@ -0,0 +1,20 @@
+package stackqueue
+
+type Stack struct {
+	items []int
+}
+
+func NewStack() *Stack {
+	return &Stack{}
+}
+
+func (s *Stack) Push(i int) {
+	s.items = append(s.items, i)
+}
+
+func (s *Stack) Pop() int {
+	l := len(s.items) - 1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
